9561ff826a736a2b0d47ef5428445c0258eda313
miko130204 <miko.business77@gmail.com>
2024-04-18 09:12:46 +0200

    added file

---
+++ Lezione2/lezione2.py
@@ -0,0 +1,16 @@
+# Mikolaj
+# 17/04/24
+print ("Salve!")
+
+
+# 2-3 Use a variable to represent a person’s name, and print a message to 
+# that person. Your message should be simple, such as, “Hello Eric, 
+# would you like to learn some Python today?”
+
+name = "Eric"
+print(f"Hello {name}, would you like to learn some Python today?")
+
+# 2-4 Use a variable to represent a person’s name, and then print 
+# that person’s name in lowercase, uppercase, and title case.
+
+print(f"name.uppercase()")
