6ea214611c8cf862d66e353f908c2721316b9dfd
miko130204 <miko.business77@gmail.com>
2024-05-08 12:54:01 +0200

    push

--- Lezione6/lezione6.py
+++ Lezione6/lezione6.py
@@ -5,9 +5,23 @@ class Person:
         self.surname: str = surname
         self.ssn: str = ssn
 
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name: str):
+        raise Exception("You cannot modify the name! ")
+    
+    def get_ssn(self):
+        return self.ssn
+
 
 
 
 person_1: Person = Person(name = "Miko", surname = "Mrc", ssn = "MRCMLJ04")
 
-print(person_1.name, person_1.surname, person_1.ssn)
+person_2: Person = Person(name = "Giuseppe", surname = "FFF", ssn = "BHO32")
+
+queue: list[Person] = [person_1, person_2]
+
+for person in queue:
+    print(person.get_ssn())
