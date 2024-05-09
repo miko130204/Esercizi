d1490e2a8e509c96438c86a07c6f5f2effeb1b08
miko130204 <miko.business77@gmail.com>
2024-05-09 13:45:28 +0200

    push

--- Lezione6/lezione6.py
+++ Lezione6/lezione6.py
@@ -111,11 +111,14 @@ class Menu:
         for food in food_list:
             print(food._price)
 
-    def get_average_price(self):
+    def get_average_price(self) -> float:
+        i: int  = 0
         sium: int = 0
         for food in food_list:
             sium += food._price
-        return sium
+            i += 1
+        avg = sium/i
+        return avg
 
 food1: Food = Food("Peach", 5, "Fruit")
 food2: Food = Food("Meat", 10, "BONO")
@@ -126,8 +129,8 @@ food5: Food = Food("Plutonio", 40, "yes")
 food_list: list = [food1, food2, food3]
 menu_fesso: Menu = Menu([food_list])
 
-print(menu_fesso.addFood(food4))
-print(menu_fesso.addFood(food5))
-print(menu_fesso.removeFood(0))
+menu_fesso.addFood(food4)
+menu_fesso.addFood(food5)
+menu_fesso.removeFood(0)
 print(menu_fesso.get_average_price())
 
