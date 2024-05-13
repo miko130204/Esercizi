a06ad28d33c4288597fc2cb54f14a6135b0f3fa5
miko130204 <miko.business77@gmail.com>
2024-05-13 13:40:07 +0200

    push

--- Lezione6/project_zoo.py
+++ Lezione6/project_zoo.py
@@ -1,41 +1,102 @@
 class Zoo:
-    def __init__():
+    def __init__(self, zoo_name: str):
+        self._zoo_name: str = zoo_name
+        self.fences = []
+        self.zoo_keepers = []
+
+    def add_fence(self, fence):
+        self._fences.append(fence)
+
+    def add_zoo_keeper(self, zoo_keeper):
+        self._zoo_keepers.append(zoo_keeper)
+
+    def feed(self):
+        for fence in self.fences:
+            fence.feed_animals()
+
+    def describe_zoo(self):
         1
 
 class Animal:
-    def __init__(self, name, specie, age, height, width, habitat):
-        self._name = name
-        self._specie = specie
-        self._age = age
-        self._height = height
-        self.width = width
-        self._habitat = habitat
-        self._health = round(100 * (1 / self._age), 3)
+    def __init__(self, name: str, specie: str, age: int, height: int, width: int, preferred_habitat: str):
+        self._name: str = name
+        self._specie: str = specie
+        self._age: int = age
+        self._height: int = height
+        self._width: int = width
+        self._preferred_habitat: str = preferred_habitat
+        self._health: float = round(100 * (1 / self._age), 3)
+        self._area: int = self._height * self._width
+        self._fence = Fence
 
 
 class Fence:
-    1
-
-class Zoo_keeper:
-    1
-
-
-
-def add_animal(animal: Animal, fence: Fence):
-    1
+    def __init__(self, name: str, area: int, temperature: int, habitat: str):
+        self._name: str = name
+        self._area: int = area
+        self._temperature: int = temperature
+        self._habitat: str = habitat
+        self._animals_list: list = []
 
 
-def remove_animal(animal: Animal, fence: Fence):
-    1
+    def add_animal(self, animal: Animal):
+        if self._area - animal._area >= 0 and animal.preferred_habitat == self.habitat:
+            animal._animals_list.append(animal)
+            self._area -= animal._area 
+
+        else: 
+            print("Can't add animal. There isn't enough space!")
+    
+
+    def remove_animal(self, animal: Animal):
+        if animal in self._animals_list:
+            self._animals_list.remove(animal)
+            self._area += animal._area
+        else:
+            print("This animal in not in this fence.")
 
 
-def feed(animal: Animal):
-    1
+    def feed_animals(self, animal : Animal):
+        for animal[Animal] in self._animals_list:
+            if self._area >= (animal._area * 1.04):
+                animal._health += (animal._health/100)*1
+                animal._area += (animal._area/100)*4
+                print(f"{animal._name} fed.")
+            else:
+                print(f"Not enough space in {self._name} to feed {animal._name}.")
 
 
-def clean(fence: Fence):
-    1
+    def clean(self, animal = Animal) -> float:
+        occupied_area = sum(animal._area for animal in self._animals_list)
+        if self._area > 0:
+            cleaning_time = occupied_area / self._area
+        else:
+            cleaning_time = occupied_area
+        print(f"{self._name} cleaned. Cleaning time: {cleaning_time} hours.")
+        return cleaning_time
+        
 
+class Zoo_Keeper:
+    def __init__(self, name: str, surname: str, id: int):
+        self._name: str = name
+        self._surname: str = surname
+        self._id: int = id
 
-def describe_zoo():
-    1
+   
+
+zoo = Zoo(name = "2M Zoo")
+fence1 = Fence(name = "Savannah", area = 1000, temperature = 40, habitat = "Grassland")
+fence2 = Fence(name = "Jungle", area = 1500, temperature = 28, habitat = "Rainforest")
+zoo.add_fence(fence1)
+zoo.add_fence(fence2)
+keeper1 = Zoo_Keeper(name = "John", surname = "Doe", id = 1)
+keeper2 = Zoo_Keeper(name = "Jane", surname = "Smith", id = 2)
+zoo.add_zoo_keeper(keeper1)
+zoo.add_zoo_keeper(keeper2)
+lion = Animal(name = "Simba", specie = "Lion", age = 5, height = 2, width = 2, habitat = "Savannah")
+elephant = Animal(name = "Dumbo", specie = "Elephant", age = 8, height = 4, width = 3, habitat = "Jungle")
+zoo.add_animal(lion, fence1)
+zoo.add_animal(elephant, fence2)
+zoo.feed()
+zoo.clean()
+zoo.describe_zoo()
