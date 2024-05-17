4d47ef334bbaf3978b46cde1275bb512b1bc9682
miko130204 <miko.business77@gmail.com>
2024-05-17 09:21:07 +0200

    push

--- Lezione6/project_zoo.py
+++ Lezione6/project_zoo.py
@@ -113,3 +113,25 @@ class Zoo_Keeper:
         self.name: str = name
         self.surname: str = surname
         self.id: int = id
+
+
+#zoo = Zoo(zoo_name = "2M Zoo")
+#fence1 = Fence(name = "Savannah", area = 200, temperature = 40, habitat = "Grassland")
+#fence2 = Fence(name = "Jungle", area = 300, temperature = 28, habitat = "Rainforest")
+#zoo.add_fence(fence1)
+#zoo.add_fence(fence2)
+#keeper1 = Zoo_Keeper(name = "John", surname = "Doe", id = 1)
+#keeper2 = Zoo_Keeper(name = "Jane", surname = "Smith", id = 2)
+#zoo.add_zoo_keeper(keeper1)
+#zoo.add_zoo_keeper(keeper2)
+#animal1 = Animal(name = "Simba", specie = "Lion", age = 5, height = 2, width = 2, preferred_habitat = "Savannah")
+#animal2 = Animal(name = "Dumbo", specie = "Elephant", age = 8, height = 4, width = 3, preferred_habitat = "Jungle")
+#zoo.add_animal(animal1, fence1)
+#zoo.add_animal(animal2, fence2)
+#zoo.add_animal(animal1, fence2)
+#zoo.add_animal(animal2, fence1)
+#animal3 = Animal(name = "Smaug", specie = "Dragon", age = 5000, height = 400, width = 300, preferred_habitat = "Savannah")
+#zoo.add_animal(animal3, fence1)
+#zoo.feed(animal1)
+#zoo.clean()
+#zoo.describe_zoo()
