7ee5ebb22144ffd167d2dbb852f5bf3c0d7319ce
miko130204 <miko.business77@gmail.com>
2024-05-30 18:34:42 +0200

    push

--- Lezione8/lezione8.py
+++ Lezione8/lezione8.py
@@ -53,9 +53,9 @@ class MathOperations:
 
 class Book:
     def __init__(self, title: str, author: str, isbn: str):
-        self.title: str = title
-        self.author: str = author
-        self.isbn: str = isbn
+        self.title = title
+        self.author = author
+        self.isbn = isbn
     
     def __str__(self):
         return f"Book(title='{self.title}', author='{self.author}', isbn='{self.isbn}')"
@@ -63,7 +63,58 @@ class Book:
     def from_string(cls, book_str: str):
         title, author, isbn = book_str.split(', ')
         return cls(title, author, isbn)
+
+class Member:
+    def __init__(self, name: str, member_id: str):
+        self.name = name
+        self.member_id = member_id
+        self.borrowed_books = []
     
+    def borrow_book(self, book: Book):
+        self.borrowed_books.append(book)
+    
+    def return_book(self, book: Book):
+        self.borrowed_books.remove(book)
+    
+    def __str__(self):
+        borrowed_books_str = ', '.join([str(book) for book in self.borrowed_books])
+        return f"Member(name='{self.name}', member_id='{self.member_id}', borrowed_books=[{borrowed_books_str}])"
+    
+    def from_string(cls, member_str: str):
+        name, member_id = member_str.split(', ')
+        return cls(name, member_id)
+
+class Library:
+    total_books = 0
+    
+    def __init__(self):
+        self.books = []
+        self.members = []
+    
+    def add_book(self, book: Book):
+        self.books.append(book)
+        Library.total_books += 1
+    
+    def remove_book(self, book: Book):
+        self.books.remove(book)
+        Library.total_books -= 1
+    
+    def register_member(self, member: Member):
+        self.members.append(member)
+    
+    def lend_book(self, book: Book, member: Member):
+        if book in self.books and member in self.members:
+            member.borrow_book(book)
+            self.remove_book(book)
+    
+    def __str__(self):
+        books_str = ', '.join([str(book) for book in self.books])
+        members_str = ', '.join([str(member) for member in self.members])
+        return f"Library(total_books={Library.total_books}, books=[{books_str}], members=[{members_str}])"
+    
+    def library_statistics(cls):
+        return f"Total number of books: {cls.total_books}"
+
 
 #Exercise 4
 
