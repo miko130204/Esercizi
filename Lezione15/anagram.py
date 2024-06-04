cf9bb90ee8afa5f04bc208fee3f456a965e6806e
miko130204 <miko.business77@gmail.com>
2024-06-04 13:30:02 +0200

    push

--- Lezione15/anagram.py
+++ Lezione15/anagram.py
@@ -8,21 +8,21 @@ def anagram(s: str, t: str) -> bool:
     sorted_t = sorted(t)
     return sorted_s == sorted_t
 
-class TestAnagramFunction(unittest.TestCase):
-    def _read_input_file(self, filename):
+class TestAnagram(unittest.TestCase):
+    def input_file(self, filename):
         with open(filename, 'r') as f:
             s = f.readline().strip()
             t = f.readline().strip()
         return s, t
 
-    def _write_output_file(self, filename, result):
+    def output_file(self, filename, result):
         with open(filename, 'w') as f:
             f.write(str(result))
 
-    def test_case_1(self):
-        s, t = self._read_input_file('test.txt')
+    def test_case(self):
+        s, t = self.input_file('test.txt')
         result = anagram(s, t)
-        self._write_output_file('output.txt', result)
+        self.output_file('output.txt', result)
         self.assertEqual(result, True)
 
 if __name__ == '__main__':
