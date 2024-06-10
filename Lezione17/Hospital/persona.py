ec029516e24cac2cebfc42c6d81e42c5eb3efd68
miko130204 <miko.business77@gmail.com>
2024-06-10 12:52:22 +0200

    push

--- Lezione17/Hospital/persona.py
+++ Lezione17/Hospital/persona.py
@@ -29,12 +29,12 @@ class Persona:
     def getName(self):
         return self.__name
 
-    def getLastname(self):
+    def getLastName(self):
         return self.__last_name
 
     def getAge(self): 
         return self.__age
 
     def greet(self):
-        return f"Ciao, sono {self.getName()} {self.getLastname()}! Ho {self.getAge()} anni!"
+        return f"Ciao, sono {self.getName()} {self.getLastName()}! Ho {self.getAge()} anni!"
                 
